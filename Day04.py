5fdab54ff3b4bb2e42c79f0d98085c3e354ca27c
ljhbt <larsjunker92@gmail.com>
2023-12-04 23:01:47 +0100

    Day 04 - in timer format

--- Day04.py
+++ Day04.py
@@ -1,4 +1,4 @@
-data = open('input.txt')
+from Timer import timer
 
 
 def count_copies(scratch_cards):
@@ -9,13 +9,28 @@ def count_copies(scratch_cards):
     return sum(count)
 
 
-cards = []
-for line in data:
-    card_data = line.split(': ')[1]
-    left_nums = [int(num) for num in card_data.split(' | ')[0].split()]
-    right_nums = [int(num) for num in card_data.split(' | ')[1].split()]
-    winning_nums = [num for num in left_nums if num in right_nums]
-    cards.append(len(winning_nums))
+def get_cards():
+    cards = []
+    for line in open('input.txt'):
+        card_data = line.split(': ')[1]
+        left_nums = [int(num) for num in card_data.split(' | ')[0].split()]
+        right_nums = [int(num) for num in card_data.split(' | ')[1].split()]
+        winning_nums = [num for num in left_nums if num in right_nums]
+        cards.append(len(winning_nums))
+    return cards
 
-print(sum(2 ** (c - 1) for c in cards if c > 0))
-print(count_copies(cards))
+
+@timer
+def part1():
+    cards = get_cards()
+    print(sum(2 ** (c - 1) for c in cards if c > 0))
+
+
+@timer
+def part2():
+    cards = get_cards()
+    print(count_copies(cards))
+
+
+part1()
+part2()
