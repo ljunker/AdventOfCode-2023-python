6b3bad923dea0559804fefdcd430f4be5a718bc0
ljhbt <larsjunker92@gmail.com>
2023-12-13 00:45:25 +0100

    Day 12 - functools.cache instead of own cache

--- Day12.py
+++ Day12.py
@@ -1,12 +1,10 @@
+from functools import cache
+
 from Timer import timer
 
-cache = {}
 
-
+@cache
 def get_ways(row, nums):
-    if (row, tuple(nums)) in cache:
-        return cache[(row, tuple(nums))]
-
     if len(nums) == 0:
         return 1 if "#" not in row else 0
 
@@ -20,12 +18,11 @@ def get_ways(row, nums):
                 (i == 0 or row[i - 1] != "#") and
                 (i + size == len(row) or row[i + size] != "#")
         ):
-            total += get_ways(row[i + size + 1:], nums[1:])
+            total += get_ways(row[i + size + 1:], tuple(nums[1:]))
 
         if row[i] == "#":
             break
 
-    cache[(row, tuple(nums))] = total
     return total
 
 
@@ -37,7 +34,7 @@ def get_total(lines, times=1):
         row, nums = line.split()
         row = "?".join([row] * times)
         nums = [int(n) for n in nums.split(",")] * times
-        total += get_ways(row, nums)
+        total += get_ways(row, tuple(nums))
     return total
 
 
