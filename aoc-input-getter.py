be72a90bf113d94e44ea65fc10c27a9c5e53e6fa
ljhbt <larsjunker92@gmail.com>
2023-12-15 21:52:57 +0100

    input-getter

---
+++ aoc-input-getter.py
@@ -0,0 +1,34 @@
+# get the input of a given year and day from advent of code
+# usage: python aoc-input-getter.py <year> <day>
+# example: python aoc-input-getter.py 2015 15
+# example: python aoc-input-getter.py 2018 1
+import os
+
+import dotenv
+import sys
+
+import requests
+
+
+def get_input(year, day):
+    dotenv.load_dotenv()
+    session = requests.Session()
+    session.cookies.update({
+        'session': os.getenv('SESSION_COOKIE')
+    })
+    r = session.get(f'https://adventofcode.com/{year}/day/{day}/input')
+    return r.text
+
+
+def main():
+    year = sys.argv[1]
+    day = sys.argv[2]
+    s = get_input(year, day)
+    # put input in "input.txt"
+    with open("input.txt", "w") as f:
+        f.write(s)
+    print(s)
+
+
+if __name__ == '__main__':
+    main()
