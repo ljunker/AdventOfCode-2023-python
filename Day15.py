eb528aff4d85a3d41333735cd095136aa97e984e
ljhbt <larsjunker92@gmail.com>
2023-12-15 12:38:55 +0100

    Day 15

--- Day15.py
+++ Day15.py
@@ -4,7 +4,7 @@ hash_char = lambda i, c: (i + ord(c)) * 17 % 256
 make_hash = lambda s: reduce(hash_char, s, 0)
 
 puzzle_input = open("input.txt").read().strip().split(",")
-print(sum(map(make_hash, puzzle_input)))
+print("part1: ", sum(map(make_hash, puzzle_input)))
 
 boxes = [dict() for _ in range(256)]
 for instruction in puzzle_input:
@@ -14,4 +14,4 @@ for instruction in puzzle_input:
         case [l]:
             boxes[make_hash(l)].pop(l, 0)
 
-print(sum(i * j * f for i, b in enumerate(boxes, 1) for j, f in enumerate(b.values(), 1)))
+print("part2: ", sum(i * j * f for i, b in enumerate(boxes, 1) for j, f in enumerate(b.values(), 1)))
